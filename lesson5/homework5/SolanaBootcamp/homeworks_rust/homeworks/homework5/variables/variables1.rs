9936767e760bf031389a95d39e13c00b271b103d
raigal-r <raquel.raigal@outlook.es>
2023-12-12 18:39:12 +0200

    session6

--- lesson5/homework5/SolanaBootcamp/homeworks_rust/homeworks/homework5/variables/variables1.rs
+++ lesson5/homework5/SolanaBootcamp/homeworks_rust/homeworks/homework5/variables/variables1.rs
@@ -2,9 +2,9 @@
 // Make me compile!
 // Execute the command `rustlings hint variables1` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
+    let y: i32;
     y = 5;
     println!("y has the value {}", y);
 }
